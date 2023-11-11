e2dd626ceb7550469ba398f8fbd86ed5c6a0831f
mrakkan <93730287+mrakkan@users.noreply.github.com>
2023-11-11 22:37:02 +0700

    newww

---
+++ todolist print/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+def show():
+#     completed = Todo.query.filter_by(complete=True).all()
+#     incomplete = Todo.query.filter_by(complete=False).all()
+#     db.session.commit()
+
+#     print(len(completed), len(incomplete))
+#     return render_template('index.html', incomplete_show=incomplete, complete_show=complete)
